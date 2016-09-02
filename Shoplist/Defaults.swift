afedb4af574aedbc7393ba6ec8f642e542521708
Olesia Kalashnik <seventhform@gmail.com>
2016-09-02 16:05:21 -0700

    Changed app icon and library icon

--- Shoplist/Defaults.swift
+++ Shoplist/Defaults.swift
@@ -9,22 +9,7 @@
 import UIKit
 
 class Defaults {
-    
-    private enum AllCategories : String {
-        case None = "None"
-        case BakedGoods = "Baked Goods"
-        case BeautyHealth = "Beauty & Health"
-        case Beverages = "Beverages"
-        case CannedGoods = "Canned Goods"
-        case Dairy = "Dairy"
-        case Dressings = "Dressings & Condiments"
-        case Produce = "Produce"
-        case HouseholdCleaning = "House & Cleaning"
-        case MeatFish = "Meat & Fish"
-        case Snacks = "Snacks"
-        case Sweets = "Sweets"
-    }
-    
+
     static let allCategories = ["None", "Baked Goods", "Beauty & Health", "Beverages", "Canned Goods", "Dairy", "Dressings & Condiments", "Household & Cleaning", "Meat & Fish", "Produce", "Snacks", "Sweets"]
     
     struct UI {
